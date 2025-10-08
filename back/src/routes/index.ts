9f664811acf3965c99c0bc89a05021aee80ad004
Christopher Pesantez <148641744+chris31p@users.noreply.github.com>
2025-10-08 10:28:56 -0500

    Revert "feat(backend): funcion de historial de busqueda"

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,25 +1,22 @@
-import { Router } from "express";
-import userRouter from "./userRoutes";
-import allianceRouter from "./allianceRoutes";
-import resourceRouter from "./resourceRoutes";
-import sectionRouter from "./sectionRoutes";
-import searchRoutes from "./searchRoutes"; 
-import { authMiddleware } from "../middlewares/auth.middleware";
+import { Router } from 'express';
+import userRouter from './userRoutes';
+import allianceRouter from './allianceRoutes';
+import resourceRouter from './resourceRoutes';
+import sectionRouter from './sectionRoutes';
+import { authMiddleware } from '../middlewares/auth.middleware';
+import { search } from '../controllers/searchController';
 
 const router = Router();
 
-// Endpoint base para verificar API
-router.get("/", (_req, res) => {
-  res.send("¡API funcionando!");
+router.get('/', (_req, res) => {
+  res.send('¡API funcionando!');
 });
 
-// Montamos el módulo de búsqueda (usa su propio router)
-router.use("/search", authMiddleware, searchRoutes);
+router.get('/search', authMiddleware, search);
 
-router.use("/users", userRouter);
-router.use("/alliances", allianceRouter);
-router.use("/resources", resourceRouter);
-router.use("/sections", sectionRouter);
+router.use('/users', userRouter);
+router.use('/alliances', allianceRouter)
+router.use('/resources', resourceRouter);
+router.use('/sections', sectionRouter);
 
 export default router;
-
