3fe4802774c7f56130d8ce88703286540bb5685d
chris31p <christopher.a31@hotmail.com>
2025-10-28 15:03:55 -0500

    fix(backend):configurar preset publico en cloudinary

--- back/src/config/cloudinary.config.ts
+++ back/src/config/cloudinary.config.ts
@@ -1,27 +1,23 @@
+// src/config/cloudinary.config.ts
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-interface CloudinaryParams {
-    folder?: string;
-    resource_type?: string;
-    public_id?: string;
-};
 
 // ✅ Almacenamiento para todo tipo de archivos (PDF, Word, PPT, etc.)
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: async (_req, file): Promise<CloudinaryParams> => ({
-        folder: 'scala_documents',
-        resource_type: 'raw', // 👈 permite subir archivos no visuales
-        public_id: `${Date.now()}_${file.originalname}`,
-    }),
+  cloudinary,
+  params: async (_req, file) => ({
+    folder: 'scala_documents',        // Carpeta opcional
+    resource_type: 'raw',             // Archivos no visuales
+    public_id: `${Date.now()}_${file.originalname}`,
+    upload_preset: 'public_raw',      // preset Unsigned
+  }),
 });
 
 export const upload = multer({ storage });
